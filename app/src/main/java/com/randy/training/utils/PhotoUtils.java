8a91f55eae298207efe19b50d4834f8e543b3451
yinzhiyu <513792933@qq.com>
2021-09-07 18:01:56 +0800

    PhotoUtils 工具类添加选择图片及文件

--- app/src/main/java/com/randy/training/utils/PhotoUtils.java
+++ app/src/main/java/com/randy/training/utils/PhotoUtils.java
@@ -29,6 +29,47 @@ import java.io.InputStream;
 public class PhotoUtils {
     private static final String TAG = "PhotoUtils";
 
+    public static final String INTENT_TYPE_IMAGE = "image/*";
+    public static final String INTENT_TYPE_FILE = "*/*";
+
+    /**
+     * 打开本地相册选择图片
+     */
+    public static void selectPic(Activity activity, String type, int requestCode) {
+        //intent可以应用于广播和发起意图，其中属性有：ComponentName,action,data等
+        Intent intent = new Intent();
+        intent.setType(type);//"image/*"
+        //action表示intent的类型，可以是查看、删除、发布或其他情况；我们选择ACTION_GET_CONTENT，系统可以根据Type类型来调用系统程序选择Type
+        //类型的内容给你选择
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        //如果第二个参数大于或等于0，那么当用户操作完成后会返回到本程序的onActivityResult方法
+        activity.startActivityForResult(intent, requestCode);
+    }
+//    /**
+//     *把用户选择的图片显示在imageview中
+//     */
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        //用户操作完成，结果码返回是-1，即RESULT_OK
+//        if(resultCode==RESULT_OK){
+//            //获取选中文件的定位符
+//            Uri uri = data.getData();
+//            Log.e("uri", uri.toString());
+//            //使用content的接口
+//            ContentResolver cr = this.getContentResolver();
+//            try {
+//                //获取图片
+//                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+//                image.setImageBitmap(bitmap);
+//            } catch (FileNotFoundException e) {
+//                Log.e("Exception", e.getMessage(),e);
+//            }
+//        }else{
+//            //操作错误或没有选择图片
+//            Log.i("MainActivtiy", "operation error");
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
     /**
      * @param activity    当前activity
      * @param imageUri    拍照后照片存储路径
