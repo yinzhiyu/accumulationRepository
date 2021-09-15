e0ae0b709565c73cdd38caccadf56d2e24ca1776
yinzhiyu <513792933@qq.com>
2021-09-15 16:24:19 +0800

    添加 kotlin demo 代码1

--- app/src/test/java/com/example/training/ExampleUnitTest.java
+++ app/src/test/java/com/example/training/ExampleUnitTest.java
@@ -1,8 +1,9 @@
 package com.example.training;
 
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
